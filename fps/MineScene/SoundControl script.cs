da5664335f0e140a93d587f9daa7dbba39c4b33f
pierrot-8 <pierrot-do8@yahoo.co.jp>
2015-02-14 23:40:15 +0900

    Sound(BGM) control class.

---
+++ fps/MineScene/SoundControl script.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+using System.Collections;
+
+public class SoundControl : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (Input.GetKeyDown (KeyCode.P)) {
+						audio.Play();
+				}
+		if (Input.GetKeyDown (KeyCode.S)) {
+			audio.Stop();
+		}
+	
+	}
+}
