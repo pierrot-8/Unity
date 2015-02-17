ada4fe9f9c2e7b8da3d7d29c06bf56591892a642
pierrot-8 <pierrot-do8@yahoo.co.jp>
2015-02-17 12:03:25 +0900

    color change(black)

---
+++ change color/black rect.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+using System.Collections;
+
+public class ColorBlack : MonoBehaviour {
+
+	public RaycastHit hit;
+	public bool flgred = false;
+	public GameObject blackrect;
+	public GameObject cube;
+	
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetMouseButton (0)) {
+			Vector3 pos = Input.mousePosition;
+			Ray ray = Camera.main.ScreenPointToRay (pos);
+			if (Physics.Raycast (ray, out hit)) {
+				blackrect = hit.collider.gameObject;
+				if (blackrect.tag == "blackrect") {
+					cube = GameObject.Find ("Cube");
+					cube.renderer.material.color = Color.black;
+				}
+			}
+		}
+		
+	}
+}
